97d6678841356be55810118f085e7e82550462db
Meder <medertaab@gmail.com>
2024-02-26 21:18:12 +0600

    Fix key props

--- components/manage/settingsNavigation/index.js
+++ components/manage/settingsNavigation/index.js
@@ -11,7 +11,7 @@ export default function SettingsNavigation() {
   return (
     <ul className="relative sm:w-[90%] sm:max-w-2xl flex m-auto text-base sm:text-lg font-semibold [&_li]:text-center [&_li]:cursor-pointer">
       {Object.keys(buttons).map((it, id) => {
-        return <SettingsButton type={it} title={buttons[it]} key={key}/>;
+        return <SettingsButton type={it} title={buttons[it]} key={id}/>;
       })}
     </ul>
   );
